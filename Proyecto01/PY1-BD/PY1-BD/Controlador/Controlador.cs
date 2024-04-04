536352e2388bc1c2eb9f419659ac4c2adde6279f
Jose Andres Solano Vargas <100001046+Jose121001@users.noreply.github.com>
2024-04-03 21:26:28 -0600

    BaseParaGerny
    CONTIENE TODA LA ESTRUCTURA DE MI PY

---
+++ Proyecto01/PY1-BD/PY1-BD/Controlador/Controlador.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PY1_BD.Controlador
+{
+    internal class Controlador
+    {
+    }
+}
