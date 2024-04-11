bd508e0b13d3b837d606470d5aec90cc868102ea
Eyden Su <137385937+Zepgi@users.noreply.github.com>
2024-04-11 01:04:44 -0600

    Modificación login

---
+++ Código/Vista/VistaLogin.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace PY1_BD {
+	partial class VistaLogin {
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VistaLogin));
+			this.MainP = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtCedula = new System.Windows.Forms.TextBox();
+			this.txtContrasena = new System.Windows.Forms.TextBox();
+			this.btIngresar = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.btSalir = new System.Windows.Forms.Button();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// MainP
+			// 
+			this.MainP.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.MainP.AutoSize = true;
+			this.MainP.BackColor = System.Drawing.Color.Transparent;
+			this.MainP.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.MainP.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.MainP.ForeColor = System.Drawing.Color.LemonChiffon;
+			this.MainP.Location = new System.Drawing.Point(177, 119);
+			this.MainP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.MainP.Name = "MainP";
+			this.MainP.Size = new System.Drawing.Size(227, 39);
+			this.MainP.TabIndex = 0;
+			this.MainP.Text = "Iniciar Sesión";
+			this.MainP.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(1139, 629);
+			this.button2.Margin = new System.Windows.Forms.Padding(4);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(100, 28);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Salir";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.LemonChiffon;
+			this.label2.Location = new System.Drawing.Point(238, 316);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(114, 21);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Contraseña:";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.Color.Transparent;
+			this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.LemonChiffon;
+			this.label3.Location = new System.Drawing.Point(251, 220);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(77, 21);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Cédula:";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// txtCedula
+			// 
+			this.txtCedula.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtCedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtCedula.Location = new System.Drawing.Point(177, 244);
+			this.txtCedula.Margin = new System.Windows.Forms.Padding(4);
+			this.txtCedula.Name = "txtCedula";
+			this.txtCedula.Size = new System.Drawing.Size(225, 26);
+			this.txtCedula.TabIndex = 6;
+			// 
+			// txtContrasena
+			// 
+			this.txtContrasena.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtContrasena.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtContrasena.Location = new System.Drawing.Point(177, 340);
+			this.txtContrasena.Margin = new System.Windows.Forms.Padding(4);
+			this.txtContrasena.Name = "txtContrasena";
+			this.txtContrasena.Size = new System.Drawing.Size(225, 26);
+			this.txtContrasena.TabIndex = 7;
+			// 
+			// btIngresar
+			// 
+			this.btIngresar.BackColor = System.Drawing.Color.Transparent;
+			this.btIngresar.BackgroundImage = global::PY1_BD.Properties.Resources.Color;
+			this.btIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btIngresar.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btIngresar.ForeColor = System.Drawing.Color.LemonChiffon;
+			this.btIngresar.Location = new System.Drawing.Point(177, 425);
+			this.btIngresar.Margin = new System.Windows.Forms.Padding(4);
+			this.btIngresar.Name = "btIngresar";
+			this.btIngresar.Size = new System.Drawing.Size(227, 46);
+			this.btIngresar.TabIndex = 8;
+			this.btIngresar.Text = "Ingresar";
+			this.btIngresar.UseVisualStyleBackColor = false;
+			this.btIngresar.Click += new System.EventHandler(this.botonIngresar_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.BackgroundImage = global::PY1_BD.Properties.Resources.Color;
+			this.pictureBox1.Image = global::PY1_BD.Properties.Resources.Logo;
+			this.pictureBox1.Location = new System.Drawing.Point(589, 11);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(552, 501);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 10;
+			this.pictureBox1.TabStop = false;
+			// 
+			// btSalir
+			// 
+			this.btSalir.BackColor = System.Drawing.Color.Transparent;
+			this.btSalir.BackgroundImage = global::PY1_BD.Properties.Resources.Color2;
+			this.btSalir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btSalir.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btSalir.ForeColor = System.Drawing.Color.LemonChiffon;
+			this.btSalir.Location = new System.Drawing.Point(1031, 518);
+			this.btSalir.Margin = new System.Windows.Forms.Padding(4);
+			this.btSalir.Name = "btSalir";
+			this.btSalir.Size = new System.Drawing.Size(101, 31);
+			this.btSalir.TabIndex = 11;
+			this.btSalir.Text = "Salir";
+			this.btSalir.UseVisualStyleBackColor = false;
+			this.btSalir.Click += new System.EventHandler(this.btSalir_Click);
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox3.BackgroundImage = global::PY1_BD.Properties.Resources.Color;
+			this.pictureBox3.Location = new System.Drawing.Point(581, -1);
+			this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(574, 568);
+			this.pictureBox3.TabIndex = 13;
+			this.pictureBox3.TabStop = false;
+			// 
+			// VistaLogin
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.BackgroundImage = global::PY1_BD.Properties.Resources.Color2;
+			this.ClientSize = new System.Drawing.Size(1148, 562);
+			this.Controls.Add(this.btSalir);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.btIngresar);
+			this.Controls.Add(this.txtContrasena);
+			this.Controls.Add(this.txtCedula);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.MainP);
+			this.Controls.Add(this.pictureBox3);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.HelpButton = true;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.MaximizeBox = false;
+			this.Name = "VistaLogin";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Hotel Karpas";
+			this.Load += new System.EventHandler(this.VistaLogin_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label MainP;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		public System.Windows.Forms.TextBox txtCedula;
+		public System.Windows.Forms.TextBox txtContrasena;
+		public System.Windows.Forms.Button btIngresar;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		public System.Windows.Forms.Button btSalir;
+		private System.Windows.Forms.PictureBox pictureBox3;
+	}
+}
+
---
+++ Código/Vista/VistaLogin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PY1_BD
+{
+    public partial class VistaLogin : Form
+    {
+        public VistaLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void botonIngresar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+		private void VistaLogin_Load(object sender, EventArgs e) {
+
+		}
+
+		private void btSalir_Click(object sender, EventArgs e) {
+			this.Close();
+		}
+
+	
+	}
+}
