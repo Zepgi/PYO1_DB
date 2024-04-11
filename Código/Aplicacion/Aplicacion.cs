59fff2802e74a4abdf2194b3b2ab93b1aa2aa5f1
Eyden Su <137385937+Zepgi@users.noreply.github.com>
2024-04-11 01:08:51 -0600

    Aplicacion para ejecutar

---
+++ Código/Aplicacion/Aplicacion.cs
@@ -0,0 +1,19 @@
+﻿using PY1_BD.Control;
+using PY1_BD.Vista;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PY1_BD.Aplicacion {
+	internal static class Aplicacion {
+		static void Main() {
+			VistaLogin vistaHotel = new VistaLogin();
+			Controlador controlador = new Controlador(vistaHotel);
+			controlador.vista.Show();
+			Application.Run(vistaHotel);
+		}
+	}
+}
