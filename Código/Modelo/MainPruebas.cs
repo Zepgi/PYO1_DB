ad344540c7ded53d33b3668e607e622ecdf743a7
Eyden Su <137385937+Zepgi@users.noreply.github.com>
2024-04-11 01:02:01 -0600

    Add files via upload

---
+++ Código/Modelo/MainPruebas.cs
@@ -0,0 +1,29 @@
+﻿using PY1_BD.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PY1_BD
+{
+    internal static class MainPruebas {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        /// 
+        /*
+        [STAThread]
+        static void Main()
+        {
+            SqlConnection bd = new SqlConnection("Data Source=EYDEN;Initial Catalog=HOTELKARPAS;Integrated Security=True");
+            Clientes c1 = new Clientes(bd);
+            //c1.agregarCliente(703100722, "Eyden Su Díaz", "Costa Rica", 84035342, "esu@estudiantec.cr");
+            c1.modificarCliente(703100722, null, null, 84035342, null);
+        }
+        */
+        
+    }
+}
